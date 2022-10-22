7970847324b8251c508b17ebc5c941f098a016d7
Aylin Bekem <bekemaylin@gmail.com>
2022-10-22 15:38:27 -0700

    third push

--- Hello.java
+++ Hello.java
@@ -7,6 +7,14 @@ public static void main(String[] args) {
 	for ( int i = 0; i<=10 ; i++){
 		System.out.println("Hello Wolrd "+ i);	
 	}
+
+
+List<String> names =new ArrayList<>(Arrays.as.List("Aylin","Engin"));
+
+	System.out.println("Hello Wolrd Names");	
+
+
+
 }
 
 }
